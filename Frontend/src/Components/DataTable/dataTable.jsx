3613d94e643b3225da553925b64abb09818a4e01
SurajMondem <surajmondem@gmail.com>
2020-03-19 16:31:12 -0400

    Faster Updates Api and Updated UI

--- Frontend/src/Components/DataTable/dataTable.jsx
+++ Frontend/src/Components/DataTable/dataTable.jsx
@@ -9,29 +9,11 @@ import Paper from '@material-ui/core/Paper';
 
 import './dataTable.css';
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData(
-        'Frozen yoghurt',
-        159,
-        6.0,
-        24,
-        4.0
-    ),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 class dataTable extends Component {
 
     constructor(props){
         super(props);
-        this.onSort=this.onSort.bind(this);
     }
 
     state = {
@@ -39,69 +21,25 @@ class dataTable extends Component {
         content: []
     };
 
-
-    onSort(event, sortKey){
-
-        const data = this.state.content;
-        console.log(sortKey + ":SortKey");
-        data.sort((a,b) => a[sortKey].toString().localeCompare(b[sortKey]).toString());
-        this.setState({data});
-    }
-
     async componentDidMount() {
-        const url = "https://coronavirus.m.pipedream.net/csse_covid_19_data/csse_covid_19_daily_reports";
+        //COUNTRY DATA
+        const url = "https://api.coronatracker.com/v2/analytics/country";
         const response = await fetch(url);
-        var data = await response.json();
-        var Names = [];
-        var uniqueData = [];
-        for(let i = 0; i < data.rawData.length; i++){
-            //console.log("Matching? :" + data.rawData[i]["Country/Region"].toString());
-            //console.log(Names.indexOf("China"));
-            //console.log(Names.indexOf(data.rawData[i]["Country/Region"]));
-            if(Names.indexOf(data.rawData[i]["Country/Region"]) === -1)
-            {
-                //console.log("Not Matched:" + data.rawData[i]["Country/Region"]);
-                Names.push(data.rawData[i]["Country/Region"]);
-                let date = data.rawData[i]['Last Update'].split("T")[0];
-                let time = data.rawData[i]['Last Update'].split("T")[1];
-                data.rawData[i]['Last Update Date'] = date;
-                data.rawData[i]['Last Update Time'] = time;
-                uniqueData.push(data.rawData[i]);
-            }
-            else {
-                console.log(uniqueData[0]);
-                for(var j=0; j<=uniqueData.length-1; j++) {
-                    console.log("uniqueData[j][\"Country/Region\"]",uniqueData[j]["Country/Region"]);
-                    console.log("data.rawData[i][\"Country/Region\"]", data.rawData[i]["Country/Region"]);
-                    if(uniqueData[j]["Country/Region"] === data.rawData[i]["Country/Region"]) {
-                      console.log("matched");
-                        uniqueData[j].Confirmed = parseInt(uniqueData[j].Confirmed) + parseInt(data.rawData[i].Confirmed);
-                        uniqueData[j].Recovered = parseInt(uniqueData[j].Recovered) + parseInt(data.rawData[i].Recovered);
-                        uniqueData[j].Deaths = parseInt(uniqueData[j].Deaths) + parseInt(data.rawData[i].Deaths);
-                    }
-                }
-            }
-        }
-        //var obj = JSON.parse(data);
+        let data = await response.json();
+        let uniqueData = [];
+        console.log(data);
 
-        // var newObj = {};
-        // for(let i in data['rawData']){
-        //     var item = data['rawData'][i];
-        //     if(newObj[item["Country/Region"]] === undefined){
-        //         newObj[item["Country/Region"]] = 0;
-        //     }
-        //     newObj[item["Country/Region"]] += item["Confirmed"];
-        // }
-        //
-        // var result = {};
-        // result.rawData = [];
-        // for(let i in newObj){
-        //     result.rawData.push({'Country/Region':i,'Confirmed ':newObj[i]});
-        // }
-        // console.log(result);
+        for(let i = 0; i < data.length; i++) {
+            let date = data[i].dateAsOf.split("T")[0];
+            let time = data[i].dateAsOf.split("T")[1];
+            data[i].dateAsOf = date;
+            data[i].timeAsOf = time;
+            uniqueData.push(data[i]);
+        }
+
         console.log(uniqueData);
 
-        this.setState({ content: uniqueData, loading: false });
+        this.setState({content: data, loading: false });
     }
 
 
@@ -109,7 +47,7 @@ class dataTable extends Component {
 
         if (this.state.loading) return <div>Loading...</div>;
         if (this.state.loading) return <div>didn't get the Data</div>;
-        console.log(this.state.content[1]["Country/Region"]);
+        console.log(this.state.content[1].countryName);
         return (
             <div className={"datatable-table"}>
                 <TableContainer className={"datatable"} component={Paper}>
@@ -117,24 +55,24 @@ class dataTable extends Component {
                         <TableHead>
                             <TableRow>
                                 <TableCell>Country</TableCell>
-                                <TableCell align="right" onClick={e => this.onSort(e, 'Confirmed')}>Confirmed</TableCell>
-                                <TableCell align="right" onClick={e => this.onSort(e, 'Deaths')}>Deaths</TableCell>
-                                <TableCell align="right" onClick={e => this.onSort(e, 'Recovered')}>Recovered</TableCell>
+                                <TableCell align="right">Confirmed</TableCell>
+                                <TableCell align="right">Deaths</TableCell>
+                                <TableCell align="right">Recovered</TableCell>
                                 <TableCell align="right">Last Updated Date</TableCell>
                                 <TableCell align="right">Last Updated Time</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
                             {this.state.content.map(row => (
-                                <TableRow key={row["Country/Region"]}>
+                                <TableRow key={row.countryName}>
                                     <TableCell component="th" scope="row">
-                                        {row["Country/Region"]}
+                                        {row.countryName}
                                     </TableCell>
-                                    <TableCell align="right">{row["Confirmed"]}</TableCell>
-                                    <TableCell id={"deaths"} align="right">{row["Deaths"]}</TableCell>
-                                    <TableCell id={"recovered"} align="right">{row["Recovered"]}</TableCell>
-                                    <TableCell align="right">{row["Last Update Date"]}</TableCell>
-                                    <TableCell align="right">{row["Last Update Time"]}</TableCell>
+                                    <TableCell align="right">{row["confirmed"]}</TableCell>
+                                    <TableCell id={"deaths"} align="right">{row["deaths"]}</TableCell>
+                                    <TableCell id={"recovered"} align="right">{row["recovered"]}</TableCell>
+                                    <TableCell align="right">{row["dateAsOf"]}</TableCell>
+                                    <TableCell align="right">{row["timeAsOf"]}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
--- Frontend/src/Components/SummaryData/DataContainers.jsx
+++ Frontend/src/Components/SummaryData/DataContainers.jsx
@@ -6,49 +6,80 @@ class DataContainers extends Component{
 
     state = {
         loading: true,
-        content: null
+        global: null,
+        country: null,
     };
 
     async componentDidMount() {
-        const url = "https://coronavirus.m.pipedream.net/csse_covid_19_data/csse_covid_19_daily_reports";
+        // FETCH GLOBAL DATA
+        const url = "https://api.coronatracker.com/v2/stats";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({ content: data.summaryStats, loading: false });
+        // this.setState({ global: data, loading: false });
+
+
+        // FETCH COUNTRY SUMMARY
+        const urlCountries = "https://api.coronatracker.com/v2/analytics/country";
+        const response2 = await fetch(urlCountries);
+        var data2 = await response2.json();
+        var Names = [];
+        var uniqueData = [];
+
+        const countrydata = [];
+        //console.log("REACHED!!");
+        if(data2) {
+            for (let i = 0; i < data2.length; i++) {
+                if (data2[i].countryCode.toString() === "CN" ||
+                    data2[i].countryCode.toString() === "US" ||
+                    data2[i].countryCode.toString() === "IN" ||
+                    data2[i].countryCode.toString() === "IT") {
+                    //console.log("DONE!!");
+                    countrydata.push(data2[i]);
+                }
+            }
+            console.log(countrydata);
+            this.setState({global: data, country: countrydata, loading: false});
+        }
     }
+
+    async getGlobalData() {
+
+    }
+
 
     render() {
         if (this.state.loading) return <div>Loading...</div>;
         if (this.state.loading) return <div>didn't get the Data</div>;
 
-        console.log(this.state.content.global);
+        console.log(this.state.global);
         return (
             <React.Fragment>
                 {/*GLOBAL DATA*/}
-                <div className={"box-container"}>
-                    <div className={"title-container"}>
+                <div className={"box-container-global"}>
+                    <div className={"title-container-global"}>
                         <div className={"title"}>GLOBAL</div>
                     </div>
-                    <div className={"data-container"}>
+                    <div className={"data-container-global"}>
                         <div className={"data-title"} >Confirmed</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"confirmed"}>
-                                {this.state.content.global.confirmed}
+                                {this.state.global.confirmed}
                             </div>
                         </div>
                     </div>
-                    <div className={"data-container"}>
+                    <div className={"data-container-global"}>
                         <div className={"data-title"} >Recovered</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"recovered"}>
-                                {this.state.content.global.recovered}
+                                {this.state.global.recovered}
                             </div>
                         </div>
                     </div>
-                    <div className={"data-container"}>
+                    <div className={"data-container-global"}>
                         <div className={"data-title"} >Deaths</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"deaths"}>
-                                {this.state.content.global.deaths}
+                                {this.state.global.deaths}
                             </div>
                         </div>
                     </div>
@@ -62,7 +93,7 @@ class DataContainers extends Component{
                         <div className={"data-title"} >Confirmed</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"confirmed"}>
-                                {this.state.content.china.confirmed}
+                                {this.state.country[0].confirmed}
                             </div>
                         </div>
                     </div>
@@ -70,7 +101,7 @@ class DataContainers extends Component{
                         <div className={"data-title"} >Recovered</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"recovered"}>
-                                {this.state.content.china.recovered}
+                                {this.state.country[0].recovered}
                             </div>
                         </div>
                     </div>
@@ -78,21 +109,21 @@ class DataContainers extends Component{
                         <div className={"data-title"} >Deaths</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"deaths"}>
-                                {this.state.content.china.deaths}
+                                {this.state.country[0].deaths}
                             </div>
                         </div>
                     </div>
                 </div>
-                {/*NON-CHINA DATA*/}
+                {/*ITALY DATA*/}
                 <div className={"box-container"}>
                     <div className={"title-container"}>
-                        <div className={"title"}>NON-CHINA</div>
+                        <div className={"title"}>ITALY</div>
                     </div>
                     <div className={"data-container"}>
                         <div className={"data-title"} >Confirmed</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"confirmed"}>
-                                {this.state.content.nonChina.confirmed}
+                                {this.state.country[1].confirmed}
                             </div>
                         </div>
                     </div>
@@ -100,7 +131,7 @@ class DataContainers extends Component{
                         <div className={"data-title"} >Recovered</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"recovered"}>
-                                {this.state.content.nonChina.recovered}
+                                {this.state.country[1].recovered}
                             </div>
                         </div>
                     </div>
@@ -108,7 +139,66 @@ class DataContainers extends Component{
                         <div className={"data-title"} >Deaths</div>
                         <div className={"data-box"}>
                             <div className={"data"} id={"deaths"}>
-                                {this.state.content.nonChina.deaths}
+                                {this.state.country[1].deaths}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*USA DATA*/}
+                <div className={"box-container"}>
+                    <div className={"title-container"}>
+                        <div className={"title"}>USA</div>
+                    </div>
+                    <div className={"data-container"}>
+                        <div className={"data-title"}>Confirmed</div>
+                        <div className={"data-box"}>
+                            <div className={"data"} id={"confirmed"}>
+                                {this.state.country[2].confirmed}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"data-container"}>
+                        <div className={"data-title"} >Recovered</div>
+                        <div className={"data-box"}>
+                            <div className={"data"} id={"recovered"}>
+                                {this.state.country[2].recovered}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"data-container"}>
+                        <div className={"data-title"} >Deaths</div>
+                        <div className={"data-box"}>
+                            <div className={"data"} id={"deaths"}>
+                                {this.state.country[2].deaths}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={"box-container"}>
+                    <div className={"title-container"}>
+                        <div className={"title"}>INDIA</div>
+                    </div>
+                    <div className={"data-container"}>
+                        <div className={"data-title"}>Confirmed</div>
+                        <div className={"data-box"}>
+                            <div className={"data"} id={"confirmed"}>
+                                {this.state.country[3].confirmed}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"data-container"}>
+                        <div className={"data-title"} >Recovered</div>
+                        <div className={"data-box"}>
+                            <div className={"data"} id={"recovered"}>
+                                {this.state.country[3].recovered}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"data-container"}>
+                        <div className={"data-title"} >Deaths</div>
+                        <div className={"data-box"}>
+                            <div className={"data"} id={"deaths"}>
+                                {this.state.country[3].deaths}
                             </div>
                         </div>
                     </div>
