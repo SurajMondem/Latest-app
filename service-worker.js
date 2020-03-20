f6f1e1523c0b52d95c2c395ce799f33070d0786b
SurajMondem <surajmondem@gmail.com>
2020-03-20 15:11:05 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18d9956db56d5ff1af4954c89c4e250e.js"
+  "/precache-manifest.042a44133919dd514de81bbdde1289bf.js"
 );
 
 self.addEventListener('message', (event) => {
