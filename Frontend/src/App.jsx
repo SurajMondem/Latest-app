91802c8b1c87aa8c211e10458aabae27a809de3b
SurajMondem <surajmondem@gmail.com>
2020-03-25 00:18:44 -0400

    Solved CSR Issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router,HashRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 //Pages
@@ -12,13 +12,13 @@ class App extends Component {
 
   render() {
     return (
-        <Router>
+        <HashRouter basename={"/"}>
             <Switch>
             <Route exact path={"/"} component={LandingPage}/>
             <Route exact path={"/stats/india"} component={IndiaStats}/>
             <Route exact path={"/stats/usa"} component={UsaStats}/>
             </Switch>
-        </Router>
+        </HashRouter>
     );
   }
 }
